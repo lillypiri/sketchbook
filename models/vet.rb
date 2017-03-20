9dc3c2eb975834d4fad2208d9b384788b6613005
Lilly Piri <lilly@lillypiri.com>
2017-03-20 20:27:08 +1000

    Lots of comments added

--- models/vet.rb
+++ models/vet.rb
@@ -1,24 +1,37 @@
 class Owner
-
+  #attr_reader is a getter
   attr_reader :pets
   attr_reader :name
-
+  # initialize is the constructor
   def initialize (name)
     @name = name
   end
 
-  def pet=(pets)
+
+  # This is the setter for pets.
+  # pets.each do |pet| looks at the pets argument (it's an array) and does something for each item, one by one.
+  # In this case, it sets each pet's owner to be self.
+  # 'self' refers to the instance variable that .pets was used with. In this case either Sue or Nathan.
+  # (refer to vet_spec.rb)
+  def pets=(pets)
     @pets = pets
     pets.each do |pet|
       pet.owner = self
     end
+  end
+
+  # We're converting an owner instance into a string.
+  # "#{variable}" in the vet_spec.rb will look in this class for what it needs to convert to a string.
+  # In this case, it's @name.
+  def to_s
+    @name
   end
 end
 
 
 
 class Pet
-
+# attr_accessor will set up a setter and a getter
   attr_accessor :owner
   def initialize(name, species)
     @name = name
--- specs/models/vet_spec.rb
+++ specs/models/vet_spec.rb
@@ -3,12 +3,22 @@ require_relative "../../models/vet"
 
 describe Owner do
   it 'play area' do
+    # def initialize (name) correlates to this.
+    # Sue is passed as the name argument.
     sue = Owner.new("Sue")
     pupper = Pet.new("Pupper", "Dog")
-    sue.pet = [pupper]
-    p sue.pets
-
-    pupper.owner = sue
-    p pupper.owner
+    nathan = Owner.new("Nathan")
+    snugglewumps = Pet.new("Snugglewumps", "Cat")
+    # Sue is an instance of owner.
+    # Sue has an instance variable called pets.
+    sue.pets = [pupper]
+    nathan.pets = [snugglewumps]
+    
+    # 'def to_s' is used to convert owner instances into strings.
+    # (In this case, their names)
+    # Anytime something is inside a #{} it calls .to_s on the instance
+    # (in this case, owner is the instance it is calling it on.)
+    puts "#{pupper.owner} is Pupper's owner."
+    puts "#{snugglewumps.owner} is Snugglewumps' servant."
   end
 end
